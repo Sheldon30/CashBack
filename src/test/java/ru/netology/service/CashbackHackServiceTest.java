090f7085faac7715d44fe76a045da3814684f500
Sheldon <aneln700@gmail.com>
2023-05-16 16:25:03 +0300

    testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,17 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+
 
 class CashbackHackServiceTest {
 
     @Test
     void remain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        Assertion
     }
 }
